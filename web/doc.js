b896c30ff093489581fce2f380f6954713183b02
Dale Weiler <weilercdale@gmail.com>
2026-07-11 06:46:07 -0400

    update

--- web/doc.js
+++ web/doc.js
@@ -16,6 +16,7 @@ const CHAPTERS = [
   { slug: 'generics',        title: 'Generics' },
   { slug: 'attributes',      title: 'Constants & Attributes' },
   { slug: 'modules',         title: 'Modules & Foreign' },
+  { slug: 'freestanding',    title: 'Freestanding Executables' },
   { slug: 'asm',             title: 'Inline Assembly' },
   { slug: 'compiler',        title: 'The Compiler' },
 ];
